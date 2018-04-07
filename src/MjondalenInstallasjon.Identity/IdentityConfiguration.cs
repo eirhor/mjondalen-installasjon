287a8c1752d75bcf75d766f1fe230162fda8b069
Eirik Horvath <eirik@geta.no>
2018-04-08 00:01:39 +0200

    Move folders to src

---
+++ src/MjondalenInstallasjon.Identity/IdentityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MjondalenInstallasjon.Identity.Data;
+using MjondalenInstallasjon.Identity.Models;
+
+namespace MjondalenInstallasjon.Identity
+{
+    public static class IdentityConfiguration
+    {
+        public static void ConfigureIdentity(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<ApplicationIdentityContext>(options => options.UseSqlServer(connectionString));
+            
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 6;
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(150);
+                options.LoginPath = "/Admin/Login";
+                options.LogoutPath = "/Admin/Logout";
+                options.AccessDeniedPath = "/Admin/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+        }
+    }
+}
