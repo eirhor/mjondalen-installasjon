475d09e53e70f408a8dba2a51803f71ee87a22d9
Eirik Horvath <eirik@geta.no>
2018-04-08 11:42:20 +0200

    Create basic admin controller.

---
+++ src/MjondalenInstallasjon.Web/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MjondalenInstallasjon.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
